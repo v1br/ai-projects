5bd0080bda36669015543a8770a26fcac95a306b
v1br <vibhorag2003@gmail.com>
2025-04-08 21:33:54 +0530

    fix: remove build errors

--- turborepo/apps/web/src/App.tsx
+++ turborepo/apps/web/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Chart } from "./pages/Chart";
 import { Error } from "./pages/Error";
 
 const App = () => {
@@ -8,7 +7,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/chart" element={<Chart />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
