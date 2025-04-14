333dacfa91a87d04c715462821d912e809b57f88
v1br <vibhorag2003@gmail.com>
2025-04-14 21:03:44 +0530

    fix: add tailwindcss plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from '@tailwindcss/vite'
 import { UserConfig } from "vite";
 
 export default defineConfig({
 	base: "./",
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	server: {
 		hmr: true,
 	},
